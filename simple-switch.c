ee120bba63fdaa221cfa3d8687055a9a8243342b
Mingxuan Zhang <146388275+AncofordAGGGGG@users.noreply.github.com>
2023-10-16 09:54:47 +0000

    Week 3 session1

---
+++ simple-switch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int a;
+    a=2;
+
+    switch (a)
+    {
+        case 0: printf ("Option 0 has been selected\n");
+        break;
+
+        case 1: printf ("Option 1 has been selected\n");
+        break;
+
+        case 2: printf ("A different option has been selection");
+    }
+}
